b927ad2bbe1a93ebe55fe913441f8b9782e4b493
Simon Zelazny <zelazny@mailbox.org>
2018-08-20 10:20:00 -0700

    Extract value in explicit step

--- app.py
+++ app.py
@@ -6,19 +6,18 @@ def application_setup(args):
     ab = wallaroo.ApplicationBuilder("Avro example app")
     ab.new_pipeline("avro example pipeline",
                     wallaroo.DefaultKafkaSourceCLIParser(decoder))
-    ab.to(pass_through)
+    ab.to(extract_value)
     ab.to_sink(wallaroo.DefaultKafkaSinkCLIParser(encoder))
     return ab.build()
 
-@wallaroo.computation(name="pass through everything")
-def pass_through(event):
-    return event
+@wallaroo.computation(name="extract reading value")
+def extract_value(reading):
+    return reading[u'val']
 
 @wallaroo.decoder(header_length=4, length_fmt=">I")
 def decoder(data):
     return deserialize(data)
 
 @wallaroo.encoder
-def encoder(data):
-    val = str(data[u'val'])
-    return (val, None, None)
+def encoder(val):
+    return (str(val), None, None)
